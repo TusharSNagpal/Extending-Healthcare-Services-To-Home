dff82afe9b5c887928a2cba32897d692e37a6802
jayantmukundam <jayantmukundam2811@gmail.com>
2023-03-15 11:33:20 +0530

    created followUp table APIs

---
+++ src/main/java/com/example/had/repositories/FieldWorkerInHospitalRepo.java
@@ -0,0 +1,7 @@
+package com.example.had.repositories;
+
+import com.example.had.entities.FieldWorkerInHospital;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FieldWorkerInHospitalRepo extends JpaRepository<FieldWorkerInHospital,Integer> {
+}
