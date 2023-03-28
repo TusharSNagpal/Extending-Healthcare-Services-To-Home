044da11f094f0d7d0438e08dff35d2976a88ca9a
jayantmukundam <62736847+jayantmukundam@users.noreply.github.com>
2023-03-28 15:02:44 +0530

    Update FieldWorkerInHospitalController.java

--- src/main/java/com/example/had/controllers/FieldWorkerInHospitalController.java
+++ src/main/java/com/example/had/controllers/FieldWorkerInHospitalController.java
@@ -15,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/fieldWorkerInHospital")
+@CrossOrigin(origins = "*")
 public class FieldWorkerInHospitalController {
     @Autowired
     FieldWorkerInHospitalService fieldWorkerInHospitalService;
