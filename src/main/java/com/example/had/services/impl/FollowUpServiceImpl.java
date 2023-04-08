7682eda3c3452c70090ccda1d286bd6e6fdb08dc
TusharSNagpal <nagpal8795@gmail.com>
2023-04-08 18:56:36 +0530

    fix-followUp-update

--- src/main/java/com/example/had/services/impl/FollowUpServiceImpl.java
+++ src/main/java/com/example/had/services/impl/FollowUpServiceImpl.java
@@ -74,7 +74,7 @@ public class FollowUpServiceImpl implements FollowUpService {
     @Override
     public void updateFollowUpByFieldWorker(FollowUpDto followUpDto, int followUpId) {
         FollowUp followUp = this.followUpRepo.findById(followUpId).orElseThrow(()-> new ResourceNotFoundException("FollowUp","followUpId",followUpId));
-        int fwInHospId = followUpDto.getFieldWorkerInHospital().getFwInHospId();
+        int fwInHospId = followUp.getFieldWorkerInHospital().getFwInHospId();
         FieldWorkerInHospital fieldWorkerInHospital = this.fieldWorkerInHospitalRepo.findById(fwInHospId).orElseThrow(()-> new ResourceNotFoundException("FollowUp","followUpId",followUpId));
         int currNumOfTasksPerDay = fieldWorkerInHospital.getNumOfTasksPerDay();
         currNumOfTasksPerDay--;
